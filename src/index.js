d0174abb9e29ab5b07568e5407e1700b3302d7e9
Guilherme Keese Zacharias <guikeese@Guilhermes-MacBook-Pro.local>
2023-01-09 09:54:46 -0300

    desafio completo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const cors = require("cors");
 
 // const { v4: uuidv4 } = require('uuid');
 
@@ -8,34 +9,122 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
-function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+function checksExistsUserAccount(req, res, next) {
+  const { username } = req.headers;
+
+  const user = users.find((users) => users.username === username);
+
+  if (!user) {
+    return res.status(400).json({ error: "usuario não cadastrado." });
+  }
+
+  req.user = user;
+
+  return next();
 }
 
-app.post('/users', (request, response) => {
-  // Complete aqui
+app.post("/users", (req, res) => {
+  const { name, username } = req.body;
+
+  //chekif exists
+  const checkUserExist = users.some((user) => user.username === username);
+
+  if (checkUserExist) {
+    return res.status(400).json({ error: "Usuario já existente." });
+  }
+
+  users.push({
+    name,
+    username,
+    id: uuidv4(),
+    todos: [],
+  });
+
+  res.status(201).send("Usuario criado com sucesso.");
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.get("/users", checksExistsUserAccount, (req, res) => {
+  const { user } = req;
+
+  return res.json(user);
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.get("/todos", checksExistsUserAccount, (req, res) => {
+  const { user } = req;
+
+  return res.json(user.todos);
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.post("/todos", checksExistsUserAccount, (req, res) => {
+  const { user } = req;
+  const { title, deadline } = req.body;
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  };
+
+  user.todos.push(todo);
+
+  return res.status(201).json(todo);
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.put("/todos/:id", checksExistsUserAccount, (req, res) => {
+  const { user } = req;
+  const { title, deadline } = req.body;
+  const { id } = req.params;
+
+  const checkTodo = user.todos.find((todo) => todo.id === id);
+
+  if (!checkTodo) {
+    return res
+      .status(400)
+      .json({ error: "todo não encontrado pela id especificada." });
+  }
+
+  checkTodo.title = title;
+  checkTodo.deadline = new Date(deadline);
+
+  return res.status(200).json(checkTodo);
 });
 
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.patch("/todos/:id/done", checksExistsUserAccount, (req, res) => {
+  const { user } = req;
+  const { id } = req.params;
+
+  const checkTodo = user.todos.find((todo) => todo.id === id);
+
+  if (!checkTodo) {
+    return res
+      .status(400)
+      .json({ error: "Nenhum todo encontrado com a ID especificada." });
+  }
+
+  checkTodo.done = true;
+
+  return res.status(201).json(checkTodo);
+});
+
+app.delete("/todos/:id", checksExistsUserAccount, (req, res) => {
+  const { user } = req;
+  const { id } = req.params;
+
+  const todoIndex = user.todos.findIndex((todo) => todo.id === id);
+
+  if (todoIndex === -1) {
+    return res
+      .status(400)
+      .json({ error: "Nenhum todo encontrado com a ID especificada." });
+  }
+
+  user.todos.splice(todoIndex, 1);
+
+  return res.status(204).json();
 });
 
 module.exports = app;
